e4583ae5fc1001f3eaefc4a0b871347010c7bd2c
NoahFCh <144356277+NoahFCh@users.noreply.github.com>
2024-03-21 21:42:55 -0600

    unable to get correct waveform

--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -58,27 +58,81 @@ architecture test_bench of thunderbird_fsm_tb is
 	
 	component thunderbird_fsm is 
 	  port(
-		
+		  i_clk, i_reset : in std_logic;
+          i_left, i_right: in std_logic;
+          o_lights_L     : out std_logic_vector(2 downto 0);
+          o_lights_R     : out std_logic_vector(2 downto 0)
 	  );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
-	
+	   signal w_clk : std_logic := '0';
+	   signal w_reset : std_logic := '0';
+	   signal w_left, w_right : std_logic := '0';
+	   
+	   signal w_lights_R, w_lights_L : std_logic_vector(2 downto 0) := "000";
 	-- constants
-	
+	   constant k_clk_period : time := 10 ns;
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	uut: thunderbird_fsm port map(
+	   i_clk => w_clk,
+	   i_reset => w_reset,
+	   i_left => w_left,
+	   i_right => w_right,
+	   o_lights_L(0) => w_lights_L(0),
+	   o_lights_L(1) => w_lights_L(1),
+	   o_lights_L(2) => w_lights_L(2),
+	   o_lights_R(0) => w_lights_R(2),
+       o_lights_R(1) => w_lights_R(1),
+       o_lights_R(2) => w_lights_R(0)
+	   
+	);
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    clk_proc : process
+        begin
+            w_clk <= '0';
+            wait for k_clk_period/2;
+            w_clk <= '1';
+            wait for k_clk_period/2;
+        end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
-	
+	sim_proc: process
+	begin
+	--sequential timing
+	   w_reset <= '1'; 
+	   wait for k_clk_period*1;
+             assert w_lights_R = "000" report "bad reset" severity failure;
+             assert w_lights_L = "000" report "bad reset" severity failure;
+      w_reset <= '0';
+      wait for k_clk_period*1;
+      
+      --lights ON
+      -- w_right <= '1'; w_left <= '1';wait for k_clk_period*1;
+        --  assert w_lights_R = "111" report "lights not properly on" severity failure;
+        --  assert w_lights_L = "111" report "lights not properly on" severity failure;
+     
+     --left lights
+     w_left <= '1';-- w_right <= '0';
+     wait for k_clk_period;
+        assert w_lights_L = "001" report "bad left signal" severity failure;
+        
+    --right lights
+    w_right <= '1'; w_left <= '0';
+    wait for k_clk_period;
+        assert w_lights_R = "001" report "bad right signal" severity failure;
+     
+     
+     
+       
+	   wait;
+	end process;
 	-----------------------------------------------------	
 	
 end test_bench;
